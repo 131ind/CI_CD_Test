4e7dfecba6474f38aaa1d6c46f43993eb1d32e67
nwellermann <nilswellermann@hs-hannover.de>
2024-08-14 14:03:01 +0200

    first commit

---
+++ src/test/java/com/example/FullStack1/FullStack1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullStack1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStack1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
